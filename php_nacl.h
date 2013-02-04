1343146831a917fedfcd7f41016251640b8a8f9d
Gasol Wu <gasol.wu@gmail.com>
2013-02-04 18:39:26 +0800

    Update author

--- php_nacl.h
+++ php_nacl.h
@@ -12,7 +12,7 @@
   | obtain it through the world-wide-web, please send a note to          |
   | license@php.net so we can mail you a copy immediately.               |
   +----------------------------------------------------------------------+
-  | Author:                                                              |
+  | Author: Gasol Wu <gasol.wu@gmai.com>                                 |
   +----------------------------------------------------------------------+
 */
 
