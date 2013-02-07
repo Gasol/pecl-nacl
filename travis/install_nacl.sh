5cf45f25b35cc31bec1391c3a84b4c0820ee146d
Gasol Wu <gasol.wu@gmail.com>
2013-02-08 01:48:26 +0800

    Move commands of nacl installation to script

---
+++ travis/install_nacl.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+wget http://hyperelliptic.org/nacl/nacl-20110221.tar.bz2
+tar jxvf nacl-20110221
+rm -rf nacl-20110221/crypto_onetimeauth/poly1305/amd64
+sed -i -e 's,$, -fPIC,' nacl-20110221/okcompilers^C nacl-20110221/okcompilers/cpp
+sed -i -e 's,$, -fPIC,' nacl-20110221/okcompilers/c nacl-20110221/okcompilers/cpp
+sed -i -e '1,3d' nacl-20110221/okcompilers/c nacl-20110221/okcompilers/cpp
+sed -i -e 's, -m32,,' nacl-20110221/okcompilers/c nacl-20110221/okcompilers/cpp
+sed -i -e 's,\(shorthostname=\).*,\1"travis",' nacl-20110221/do
+cd nacl-20110221/
+./do
+arch=`build/travis/bin/okabi`
+ar -r build/travis/lib/$arch/libnacl.a build/travis/lib/$arch/randombytes.o
