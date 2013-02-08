b13b8585530db18514abed1a09d1f16f7253ec50
Gasol Wu <gasol.wu@gmail.com>
2013-02-08 11:07:44 +0800

    typo

--- travis/install_nacl.sh
+++ travis/install_nacl.sh
@@ -3,7 +3,7 @@
 wget http://hyperelliptic.org/nacl/nacl-20110221.tar.bz2
 tar jxvf nacl-20110221.tar.bz2
 rm -rf nacl-20110221/crypto_onetimeauth/poly1305/amd64
-sed -i -e 's,$, -fPIC,' nacl-20110221/okcompilers^C nacl-20110221/okcompilers/cpp
+sed -i -e 's,$, -fPIC,' nacl-20110221/okcompilers/c nacl-20110221/okcompilers/cpp
 sed -i -e 's,$, -fPIC,' nacl-20110221/okcompilers/c nacl-20110221/okcompilers/cpp
 sed -i -e '1,3d' nacl-20110221/okcompilers/c nacl-20110221/okcompilers/cpp
 sed -i -e 's, -m32,,' nacl-20110221/okcompilers/c nacl-20110221/okcompilers/cpp
